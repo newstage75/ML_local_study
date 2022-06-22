caec78e4ad53288f315c631a11a4c70b46f06752
github newstage75 <rightkingdxanntume@gmail.com>
2022-06-22 20:33:03 +0900

    タイポ修正#2

--- ZeroTuku1/ch03/neuralnet_mnist.py
+++ ZeroTuku1/ch03/neuralnet_mnist.py
@@ -33,8 +33,8 @@ network = init_network()
 accuracy_cnt = 0
 for i in range(len(x)):
     y = predict(network, x[i])
-    p = np.arfmax(y) #最も確率の高い要素のインデックスを取得
+    p = np.argmax(y) #最も確率の高い要素のインデックスを取得
     if p == t[i]:
         accuracy_cnt += 1
 
-print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+print("Accuracy:" + str(float(accuracy_cnt) / len(x))) #0.9352
