e105921020e011752310aecb16d268085782e7ab
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2024-11-25 07:37:24 +0900

    勾配確認による自動テスト

--- Zerotuku3/steps/step10.py
+++ Zerotuku3/steps/step10.py
@@ -92,3 +92,12 @@ class SquareTest(unittest.TestCase):
         y.backward()
         expected = np.array(6.0)
         self.assertEqual(x.grad, expected)
+    
+    def test_gradient_check(self):
+        x = Variable(np.random.rand(1))
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
+    
