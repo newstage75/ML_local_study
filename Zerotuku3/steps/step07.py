91aceef305cb135d9d67aefa82bf3d180a17339e
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2024-11-24 09:34:21 +0900

    ステップ７_バックプロパゲーションの自動化
    1_逆伝搬の自動化のために

---
+++ Zerotuku3/steps/step07.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+        self.grad = None
+        self.creator = None
+        
+    def set_creator(self, func):
+        self.creator = func
+
+
+class Function:
+    def __call__(self, input): # __call__メソッドは、Pythonの特殊なメソッド。インスタンスを関数のように呼び出せるようにする。
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        output.set_creator(self) # Set parent function
+        self.input = input # Remember the input variable
+        self.output = output
+        return output
+    
+    def forward(self,x):
+        raise NotImplementedError()
+    
+    def backward(self, gy):
+        raise NotImplementedError()
+    
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+    
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+   
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+    
+    
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+#逆向きに計算グラフのノードを辿る
+assert y.creator == C
+assert y.creator.input == b
+assert y.creator.input.creator == B
+assert y.creator.input.creator.input == a
+assert y.creator.input.creator.input.creator == A
+assert y.creator.input.creator.input.creator.input == x
