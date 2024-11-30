5f1d8da0c9d14a9d19f8fc814d53556dc24f64df
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2024-11-30 09:59:54 +0900

    ステップ１８＿メモリを減らすモード
    Configでモードを切り替える

---
+++ Zerotuku3/steps/step18.py
@@ -0,0 +1,125 @@
+import weakref
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+            
+        self.data = data
+        self.grad = None
+        self.creator = None
+        self.generation = 0 # generationの初期化
+    
+    def cleargrad(self):
+        self.grad = None
+    
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1 #　親世代+1
+        
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+            
+        funcs = []
+        seen_set = set()
+        
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+                
+        add_func(self.creator)
+        
+        while funcs:
+            f = funcs.pop()
+            gyx = [output().grad for output in f.outputs] # 出力変数であるoutputsのgrad（微分）をリストにまとめる
+            gxs = f.backward(*gyx) # 関数fの逆伝搬を呼び出す
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+            
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+                
+                if x.creator is not None:
+                    add_func(x.creator)
+            
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None # yはweakref。デフォルトでretain_grad=Falseの場合、途中の変数の微分はリセットされる。
+
+                
+def as_array(x):
+    if np.isscalar(x): #スカラーの場合はnp.array()である多次元配列の形式に変換する。
+        return np.array(x)
+    return x
+
+class Config:
+    enable_backprop = True # Trueの場合は逆伝搬を行う。Falseの場合は逆伝搬を行わない。
+
+class Function:
+    #__call__メソッドで出力変数に対して、いくつかの設定を行う
+    def __call__(self, *inputs): # *inputs（頭にアスタリスク）で可変長引数を受け取る
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs) # *xs（頭にアスタリスク）でリストの要素を展開して渡す（アンパッキング）
+        if not isinstance(ys, tuple): #タプルでない場合はタプルに変換
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+        
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs]) # 世代の設定
+            for output in outputs:
+                output.set_creator(self) # つながりの設定
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+            
+        
+        # リストの要素が1つの場合はその要素を返す
+        return outputs if len(outputs) > 1 else outputs[0]
+    
+    def forward(self,xs):
+        raise NotImplementedError()
+    
+    def backward(self, gys):
+        raise NotImplementedError()
+    
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+    
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        # print('Add backward gy:', gy) # gyは1.0？
+        return gx
+    
+
+def square(x):
+    return Square()(x)
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return (y)
+    
+    def backward(self, gy): #上流の微分を"そのまま流す"のが足し算の逆伝搬
+        return gy, gy
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
+Config.enable_backprop = True
+x = Variable(np.ones((100, 100, 100)))
+y = square(square(square(x)))
+# この逆伝搬を行う場合は、enable_backpropをTrueにする必要がある。
+# Falseの場合、途中の計算結果は使用されたあとにリセットされる。
+y.backward() 
