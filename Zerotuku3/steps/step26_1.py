e7e4e508c17cf7559ef6d0fadafdc7b76223df35
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2024-12-07 22:01:11 +0900

    ステップ２６_計算グラフの可視化

---
+++ Zerotuku3/steps/step26_1.py
@@ -0,0 +1,22 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+from dezero.utils import _dot_var, _dot_func
+
+#使用例
+x = Variable(np.random.randn(2,3))
+x.name = 'x'
+print(_dot_var(x)) 
+print(_dot_var(x, verbose=True))
+
+print('----------------------------------------')
+
+#使用例
+x0 = Variable(np.array(1.0))
+x1 = Variable(np.array(1.0))
+y = x0 + x1
+txt = _dot_func(y.creator)
+print(txt)
