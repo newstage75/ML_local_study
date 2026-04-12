93bcb1a82ab436489226606daef14d430aabc636
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2026-04-12 15:00:09 +0900

    数式の書き換え
    １つ前の推定値から、新しい推定値へ

--- Zerotuku4/ch01/avg.py
+++ Zerotuku4/ch01/avg.py
@@ -3,8 +3,14 @@ import numpy as np
 np.random.seed(0)
 rewards = []
 
+# for n in range(1,11):
+#     reward = np.random.rand() # ダミーの報酬
+#     rewards.append(reward)
+#     Q = sum (rewards) /n
+#     print(Q)
+    
+Q = 0
 for n in range(1,11):
-    reward = np.random.rand() # ダミーの報酬
-    rewards.append(reward)
-    Q = sum (rewards) /n
+    reward = np.random.rand()
+    Q = Q + (reward - Q)/n
     print(Q)
