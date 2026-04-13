38bb45ff3e75ae0351c759f34c2cb2ef998a619d
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2026-04-13 18:01:16 +0900

    非定常問題として解く

--- Zerotuku4/ch01/non_stationbary.py
+++ Zerotuku4/ch01/non_stationbary.py
@@ -12,3 +12,56 @@ class NonStatBandit:
             return 1
         else:
             return 0
+
+# p35-1
+class AlphaAgent:
+    def __init__(self, epsilon, alpha, actions=10):
+        self.epsilon = epsilon
+        self.Qs = np.zeros(actions)
+        self.alpha = alpha
+    
+    def update(self, action, reward):
+        # alphaで更新
+        self.Qs[action] += (reward - self.Qs[action]) + self.alpha
+    
+    def get_action(self):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(0, len(self.Qs))
+        return np.argmax(self.Qs)
+    
+# 実行する
+import numpy as np
+import matplotlib.pyplot as plt # matplotlibのインポート
+
+steps = 1000
+epsilon = 0.1
+alpha = 0.8
+
+bandit = NonStatBandit()
+agent = AlphaAgent(epsilon, alpha)
+total_reward = 0
+total_rewards = []
+rates = []
+
+for step in range(steps):
+    action = agent.get_action()  # ①行動を選ぶ
+    reward = bandit.play(action) # ②実際にプレイして報酬を得る。
+    agent.update(action, reward) # ③行動と報酬から学ぶ
+    total_reward += reward
+    
+    total_rewards.append(total_reward)
+    rates.append(total_reward / (step+1))
+    
+print(total_reward)
+
+# グラフの描画(1)
+plt.ylabel('Total reward')
+plt.xlabel('Steps')
+plt.plot(total_rewards)
+plt.show()
+
+# グラフの描画(2)
+plt.ylabel('Rates')
+plt.xlabel('Steps')
+plt.plot(rates)
+plt.show()
